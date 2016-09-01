29e6d1295548b1a07a71394285e43e19e62a7134
Perroud, Benoit <benoit@noisette.ch>
2016-09-01 21:25:59 +0200

    Working on pipeline parts

---
+++ src/main/java/ch/daplab/yarn/TransactionToJson.java
@@ -0,0 +1,25 @@
+package ch.daplab.yarn;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import rx.functions.Func1;
+import ch.daplab.jubilantoctoumbrella.Transaction;
+
+/**
+ * Created by bperroud on 01/09/16.
+ */
+public class TransactionToJson implements Func1<Transaction, byte[]> {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public byte[] call(Transaction tx) {
+
+        try {
+            return mapper.writeValueAsBytes(tx);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Oups", e);
+        }
+
+    }
+}
