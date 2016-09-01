8a6921f1d4f0ef41e579d2c04bf51a856d58f085
Aubry Cholleton <aubry.cholleton@gmail.com>
2016-09-01 21:24:04 +0200

    add unit test. fix generator to complete at some point.

--- src/main/ch/daplab/jubilantoctoumbrella/EventGenerator.java
+++ src/main/ch/daplab/jubilantoctoumbrella/EventGenerator.java
@@ -4,21 +4,25 @@ import rx.Observable;
 
 public class EventGenerator {
 	private RandomGeneratorService generator;
+
 	public EventGenerator(RandomGeneratorService generator) {
 		this.generator = generator;
 	}
-	
+
 	public Observable<Transaction> getTransactions() {
 		return Observable.create(observer -> {
 			try {
-	            if (!observer.isUnsubscribed()) {
-	                while (true) {
-	                    observer.onNext(generator.getRndTransaction());
-	                }
-	            }
-	        } catch (Exception e) {
-	            observer.onError(e);
-	        }
+				while (true) {
+					if (!observer.isUnsubscribed()) {
+						observer.onNext(generator.getRndTransaction());
+					} else {
+						observer.onCompleted();
+						break;
+					}
+				}
+			} catch (Exception e) {
+				observer.onError(e);
+			}
 		});
 	}
 }
