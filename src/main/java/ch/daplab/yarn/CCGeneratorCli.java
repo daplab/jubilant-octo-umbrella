950d968a6976dd1c87742d7d9f39fe26b1155156
Perroud, Benoit <benoit@noisette.ch>
2016-09-04 08:48:48 +0200

    Twill Tryout...

--- src/main/java/ch/daplab/yarn/CCGeneratorCli.java
+++ src/main/java/ch/daplab/yarn/CCGeneratorCli.java
@@ -1,9 +1,14 @@
 package ch.daplab.yarn;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Futures;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.twill.api.ClassAcceptor;
 import org.apache.twill.api.TwillController;
 import org.apache.twill.api.TwillRunnerService;
 import org.apache.twill.api.logging.PrinterLogHandler;
@@ -12,6 +17,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.PrintWriter;
+import java.net.URL;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class CCGeneratorCli extends AbstractAppLauncher {
@@ -31,8 +38,17 @@ public class CCGeneratorCli extends AbstractAppLauncher {
         runnerService.start();
 
 
+        YarnConfiguration yarnConfiguration = new YarnConfiguration(getConf());
+        String yarnClasspath =
+                yarnConfiguration.get(YarnConfiguration.YARN_APPLICATION_CLASSPATH,
+                        Joiner.on(",").join(YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH));
+        List<String> applicationClassPaths = Lists.newArrayList();
+        Iterables.addAll(applicationClassPaths, Splitter.on(",").split(yarnClasspath));
+
         final TwillController controller = runnerService.prepare(new CCGeneratorTwillGenerator())
                 .addLogHandler(new PrinterLogHandler(new PrintWriter(System.out)))
+                .withApplicationClassPaths(applicationClassPaths)
+                .withBundlerClassAcceptor(new HadoopClassExcluder())
                 .start();
 
 
@@ -55,6 +71,15 @@ public class CCGeneratorCli extends AbstractAppLauncher {
 
         // Good, let it simply run on his own.
         return ReturnCode.ALL_GOOD;
+    }
+
+    static class HadoopClassExcluder extends ClassAcceptor {
+        @Override
+        public boolean accept(String className, URL classUrl, URL classPathUrl) {
+            // exclude hadoop but not hbase package
+            System.out.println(className + ", classUrl=" + className.toString() + ", classPathUrl=" + classPathUrl.toString());
+            return !(className.startsWith("org.apache.hadoop") && !className.startsWith("org.apache.hadoop.hbase"));
+        }
     }
 
 }
